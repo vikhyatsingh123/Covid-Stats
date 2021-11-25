b891e0d0494b99f0a18107e516e3d782cd81b6d9
vikhyat singh <vikhyatsingh628@gmail.com>
2021-11-25 14:03:49 +0530

    Updates

---
+++ precache-manifest.2e641add9c4248f867134d059d8c8a46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38858ea2271d94f18319de2cda7e2b1b",
+    "url": "/Covid-Stats/index.html"
+  },
+  {
+    "revision": "821b78950f39d364a4c7",
+    "url": "/Covid-Stats/static/css/main.b06fe620.chunk.css"
+  },
+  {
+    "revision": "50e02d0094b78c6e8602",
+    "url": "/Covid-Stats/static/js/2.25b8d782.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-Stats/static/js/2.25b8d782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821b78950f39d364a4c7",
+    "url": "/Covid-Stats/static/js/main.3096ec47.chunk.js"
+  },
+  {
+    "revision": "073e94b59c2a7f036a10",
+    "url": "/Covid-Stats/static/js/runtime-main.b27d1e8e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Stats/static/media/image.d7265326.png"
+  }
+]);
